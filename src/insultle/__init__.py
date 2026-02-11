33b8b3ef721b54bf524e043564ffd380730bcb89
qqiann <zhangqianqian80@gmail.com>
2026-02-11 12:49:43 +0100

    sfondo finito

--- src/insultle/__init__.py
+++ src/insultle/__init__.py
@@ -1,6 +1,3 @@
-def main() -> None:
-    print("Hello from insultle!")
-
 import pygame
 
 pygame.init()
@@ -11,11 +8,17 @@ Altezza_Schermo = 745
 schermo = pygame.display.set_mode((Larghezza_Schermo, Altezza_Schermo)) 
 pygame.display.set_caption("Insultle") 
 
-#imgSfondo = pygame.image.load("sfondoINSULTLE.png") devo aggiungre il file dopo
-imgSfondo = pygame.transform.scale(imgSfondo,(SCREEN_WIDTH,SCREEN_HEIGHT))
+imgSfondo = pygame.image.load("sfondoINSULTLE.jpg") 
+imgSfondo = pygame.transform.scale(imgSfondo,(Larghezza_Schermo,Altezza_Schermo))
+
+FontTitolo = pygame.font.SysFont('Impact', 50)
+FontoLettere = pygame.font.SysFont('Impact', 20)
+
+Titolo = FontTitolo.render("INSULTLE", True, "#B5E61D")
+letteraA = FontoLettere.render("A", True, "black")
 
 running = True
-#LetteraA = False
+LetteraA = False
 
 while running:
     for event in pygame.event.get():
@@ -26,9 +29,13 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
                 
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_a:
-#                 LetteraA = True 
-                
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                LetteraA = True 
+        
+        schermo.blit(imgSfondo,(0,0) )
+        schermo.blit(Titolo, (50,50))
+        schermo.blit(letteraA, (15,25))
+        pygame.display.flip()
+        
 pygame.quit()
